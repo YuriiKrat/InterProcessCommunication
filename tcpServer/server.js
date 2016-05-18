c62af1d38f7e413284153b8b40a807b37373ff8d
YuriiKrat <yuriikrat97@gmail.com>
2016-05-19 01:12:16 +0300

    Lab3 is done

--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,13 +1,35 @@
 var api = {};
 global.api = api;
 api.net = require('net');
+api.os = require('os');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var cpuNumber = api.os.cpus().length;
+var parts = [];
+var clients = [];
 
-var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
-  console.log('Connected: ' + socket.localAddress);
-  socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
-  });
+var part = task.length / cpuNumber;
+
+var server = api.net.createServer(function (socket) {
+
+    if (clients.length == cpuNumber) {
+        process.exit(1);
+    }
+
+    clients.push(socket);
+    var count = clients.length;
+
+    var startIndex = part * (clients.length - 1);
+    
+    socket.write(JSON.stringify(task.slice(startIndex, startIndex + part)));
+
+    socket.on('data', function (data) {
+        console.log('Result recieved by server:' + data);
+        parts[count] = JSON.parse(data);
+        var result = [];
+   		parts.forEach(function (res) {
+        	result = result.concat(res);
+    });
+        console.log("General result is: " + result);
+    });
 }).listen(2000);
