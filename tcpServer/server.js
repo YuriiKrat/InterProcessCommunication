7ac717a8c9b8e583ed20349864d0f4fb5b5c684b
YuriiKrat <yuriikrat97@gmail.com>
2016-05-19 16:14:43 +0300

    New task added

--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,35 +1,46 @@
-var api = {};
-global.api = api;
-api.net = require('net');
-api.os = require('os');
+var api = {}; 
+global.api = api; 
+api.net = require('net'); 
+api.os = require('os'); 
 
-var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
-var cpuNumber = api.os.cpus().length;
-var parts = [];
-var clients = [];
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11]; 
+var cpuNumber = api.os.cpus().length; 
+var parts = []; 
+var clients = []; 
+var result = []; 
+var part = task.length / cpuNumber; 
+var server = api.net.createServer(function(socket) { 
 
-var part = task.length / cpuNumber;
+	socket.id = clients.length; 
 
-var server = api.net.createServer(function (socket) {
+	var count = clients.length; 
 
-    if (clients.length == cpuNumber) {
-        process.exit(1);
-    }
+	var startIndex = 0; 
+	clients.push(socket); 
+	if (clients.length == cpuNumber) { 
+		for (var i = 0; i < clients.length; i++) { 
+			clients[i].write(JSON.stringify(task.slice(startIndex, startIndex + part))); 
+			startIndex = part * (i+1); 
+		} 
+	} 
 
-    clients.push(socket);
-    var count = clients.length;
-
-    var startIndex = part * (clients.length - 1);
-    
-    socket.write(JSON.stringify(task.slice(startIndex, startIndex + part)));
-
-    socket.on('data', function (data) {
-        console.log('Result recieved by server:' + data);
-        parts[count] = JSON.parse(data);
-        var result = [];
-   		parts.forEach(function (res) {
-        	result = result.concat(res);
-    });
-        console.log("General result is: " + result);
-    });
+	socket.on('data', function(data) { 
+		console.log('Result recieved by server:' + data); 
+		var res = {}; 
+		res.data = JSON.parse(data); 
+		res.id = socket.id; 
+		parts[count] = res; 
+		result = [];
+		if (clients.length == cpuNumber) { 
+		parts.sort(function(a, b) { 
+			if (a.id < b.id) 
+				return -1; 
+			else 
+				return 1; 
+			}).forEach(function(res) { 
+			result = result.concat(res.data); 
+		}); 
+	}
+		console.log("General result is: " + result); 
+	}); 
 }).listen(2000);
